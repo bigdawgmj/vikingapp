4a18d4a26902fe01717c4cd1b06ce403fb479df0
Mark Jensen <mjensen@co.davis.ut.us>
2018-06-12 11:12:22 -0600

    Update angular to remove dependency vulnerabilty

--- vikings/src/app/vsts/services/test.service.ts
+++ vikings/src/app/vsts/services/test.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs/Observable';
-import 'rxjs/add/observable/of';
+import { Observable } from 'rxjs';
+
 
 import { Teammate } from '../model/teammate';
 import { Weather } from '../model/weather';
